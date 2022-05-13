976e31fcbc328a3e472ba142c1bd5ccc91f98299
Rick Lesinszki <rlesinszki@gmail.com>
2022-05-13 02:42:24 -0400

    Added time

--- hello-world.c
+++ hello-world.c
@@ -3,9 +3,19 @@
 */
 
 #include <stdio.h>
+#include <time.h>
+#include <unistd.h> // Sleep function.
 
 int main(int argc, char **argv) {
-  printf("Hello World\n");
+  printf("Hello World\n");  
+  
+  time_t now = time(NULL);
+  printf("%ld\n", now); // # of seconds since Jan. 1, 1970 midnight.
+  
+
+  
+  // struct tm *gm_time = gmtime(&now);
+  
 }
 
 
